49ebd7610db61dda4766e3711bfe57b40c59b571
Ashley Livingston <ashleyhlivingston@gmail.com>
2019-06-24 12:23:27 -0700

    Updates

---
+++ precache-manifest.024c195d9d6b5383faa71a9eb3f23866.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b33e1377ead919b8a02ccacf65cae04b",
+    "url": "/simon-says/index.html"
+  },
+  {
+    "revision": "7eef74682d2a11e38f64",
+    "url": "/simon-says/static/css/main.23903b9f.chunk.css"
+  },
+  {
+    "revision": "5b537948f24afc95a481",
+    "url": "/simon-says/static/js/2.5dbd8ed4.chunk.js"
+  },
+  {
+    "revision": "7eef74682d2a11e38f64",
+    "url": "/simon-says/static/js/main.fd4d7ee0.chunk.js"
+  },
+  {
+    "revision": "2ff488b8d7e0b2971c4e",
+    "url": "/simon-says/static/js/runtime~main.4578f539.js"
+  }
+]);
